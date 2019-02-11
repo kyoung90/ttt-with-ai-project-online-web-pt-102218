ea530fcf70ce929351c3570bb2fffcdd9f85ba6f
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-02-11 23:42:18 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -68,7 +68,7 @@ class Game
     input = current_player.move(self.board)
     if self.board.valid_move?(input)
       # binding.pry
-      self.board.position(input) = self.current_player.token
+      # self.board.position(input) = self.current_player.token
     else
       turn
     end
